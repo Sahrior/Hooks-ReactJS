80d54ba7d76e30c42996c08fa9703f9baf5790ca
Md Sahrior Haider Shovon <165468630+Sahrior@users.noreply.github.com>
2026-03-05 01:15:21 +0600

    use state full done

--- Hooks/src/UseState.jsx
+++ Hooks/src/UseState.jsx
@@ -3,18 +3,44 @@ import { useState } from 'react'
 
 const UseState = () => {
 
-    const [num, setnum] = useState(0)
 
+    {/* Basic useState */}
+    const [num, setnum] = useState(0)
     function ChangeNum(){
         setnum(num+1);
     }
 
+    {/*useState using Object */}
+    const [car, setCar] = useState({
+        brand: "Ferrari",
+        model: "Roma",
+        year: "2023",
+        color: "red"
+    });
+    const changeColor =()=>{
+        setCar((prev)=>{
+            return{...prev, color: "blue"}
+        })
+    }
+
 
   return (
-    <div>
-        <h1>Counter is {num}</h1>
-        <button onClick={ChangeNum} >Change</button>
-    </div>
+    <>
+        {/* Basic useState */}
+        <div>
+            <h1>Counter is {num}</h1>
+            <button onClick={ChangeNum} >Change</button>
+        </div>
+
+        {/*useState using Object */}
+        <div>
+            <h1>My {car.brand}</h1>
+            <h2>It is a {car.color} {car.model} from {car.year}</h2>
+            <button onClick={changeColor} >Blue</button>
+        </div>
+
+
+    </>
   )
 }
 
